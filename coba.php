2f6435936852258aa8f56e3ca2a09dabd07de60f
Farkhan <e41230747@student.polije.ac.id>
2024-08-30 08:20:22 +0700

    Belajar

---
+++ coba.php
@@ -0,0 +1,17 @@
+<?php
+
+$tugas1 = 90;
+$tugas2  = 80;
+$jumlah = $tugas1 + $tugas2;
+$rerata = $jumlah/2;
+
+$gabungan = (string)$tugas1 . (string)$tugas2; 
+
+
+echo "Tugas I : " . $tugas1."<br>";
+echo "Tugas II : " . $tugas2."<br>";
+echo "Jumlah Tugas : ".$jumlah."<br>";
+echo "Rerata Tugas : ".$rerata."<br>";
+echo "Gabungan String Tugas: " . $gabungan; 
+echo "123432233"
+?>
