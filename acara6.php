0d1599fdb043cdfc365a80d706bed34b685f8b0e
developer <shashava05@gmail.com>
2024-09-04 15:16:26 +0700

    update acara6

--- acara6.php
+++ acara6.php
@@ -16,9 +16,9 @@ $B = [
 // Matriks untuk menyimpan hasil penjumlahan
 $C = [];
 
-// Looping untuk menjumlahkan elemen-elemen dari matriks A dan B
-for ($i = 0; $i < 3; $i++) {
-    for ($j = 0; $j < 3; $j++) {
+// Looping bersarang untuk menjumlahkan elemen-elemen dari matriks A dan B
+for ($i = 0; $i < 3; $i++) { //berfungsi untuk mengakses setiap baris dari matriks.
+    for ($j = 0; $j < 3; $j++) { //berfungsi untuk mengakses setiap kolom di dalam baris yang sedang diproses.
         // Menjumlahkan elemen yang bersesuaian dari matriks A dan B
         $C[$i][$j] = $A[$i][$j] + $B[$i][$j];
     }
